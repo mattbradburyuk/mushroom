027290062364ddd8c49efadadc6ea77456f04f7c
mattbradburyuk <matthew.bradbury@me.com>
2016-09-29 10:34:08 +0100

    tidied up after update, ready to push to github

--- sources/example_child.sol
+++ sources/example_child.sol
@@ -1,28 +1,36 @@
 pragma solidity ^0.4.1;
 
-contract base_contract{
+contract Base_contract{
 
     uint base_value;
 
-    function base_contract(){
+    function Base_contract(){
         base_value = 100;
     }
 
     function get_base_value() constant returns (uint){
         return base_value;
     }
+
+    function set_base_value(uint val) {
+        base_value = val;
+    }
 }
 
 
-contract child_contract is base_contract {
+contract Child_contract is Base_contract {
 
     uint child_value;
 
-    function child_contract(){
+    function Child_contract(){
         child_value = 200;
     }
 
     function get_child_value() constant returns (uint){
         return child_value;
+    }
+
+    function set_child_value(uint val) {
+        child_value = val;
     }
 }
--- sources/example_grandchild.sol
+++ sources/example_grandchild.sol
@@ -2,15 +2,19 @@ pragma solidity ^0.4.1;
 
 import "example_child.sol";
 
-contract grandchild_contract is child_contract {
+contract Grandchild_contract is Child_contract {
 
     uint grandchild_value;
 
-    function grandchild_contract(){
+    function Grandchild_contract(){
         grandchild_value = 300;
     }
 
     function get_grandchild_value() constant returns (uint){
         return grandchild_value;
+    }
+
+    function set_grandchild_value(uint val) {
+        grandchild_value = val;
     }
 }
