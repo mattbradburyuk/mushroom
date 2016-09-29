932e42d587032156a83f55d7c21f8ee67325fa3b
mattbradburyuk <matthew.bradbury@me.com>
2016-09-29 13:13:41 +0100

    commit before try out cp commands

--- mushroom_init.sh
+++ mushroom_init.sh
@@ -12,51 +12,69 @@
 
 DIR_NAME=${PWD##*/}
 if [ $DIR_NAME != "mushroom" ];
-    then echo 'not in mushroom folder please run script from mushroom folder'
+    then echo ' ---> Not in mushroom folder please run script from mushroom folder'
     exit
 fi
 
-echo 'through if'
+# check that the mushroom_template_files directory is present
 
-echo arg 0: $0
-echo arg 1: $1
-echo arg 2: $2
+if [ `find . -maxdepth 1 -type d| grep mushroom_template_files` = './mushroom_template_files' ]; then
+        echo " ---> mushroom_template_files directory found"
+    else
+        echo echo " ---> current folder doesn't contain mushroom_template_files directory"
+        exit
+fi
+
+
+# Set SOURCE_DIR for template files
+
+SOURCE_DIR=$PWD/mushroom_template_files
+echo  ' ---> Source directory: ' $SOURCE_DIR
+
+
+# check number of args > 3
+
+if [ "$#" -gt 2 ];
+    then echo ' ---> Expecting only three arguments: $ mushroom_init <flag> <new project name and path> '
+    exit
+fi
+
+
+# check that referencing flag is present
 
 case $1 in
-    -r) echo "Using relative referencing from mushroom folder to new project folder"
+    -r) echo " ---> Using relative referencing from mushroom folder to new project folder"
         TARGET_DIR=$PWD'/'$2;;
-    -a) echo "Using absolute referencing to project folder"
+    -a) echo " ---> Using absolute referencing to project folder"
         TARGET_DIR=$2;;
      *) echo '***** add usage instructions ******'
         exit;;
 esac
 
+
+
 case $2 in
-    '') echo 'Path to new project directory (arg 2) is blank, cannot use mushroom director, it would get messy'
+    '') echo ' ---> Path to new project directory (arg 2) is blank, cannot use mushroom directory, it would get messy'
         exit;;
 
-    .) echo 'Cannot use mushroom directory, it would get messy'
+    .) echo ' ---> Cannot use mushroom directory, it would get messy'
        exit;;
 esac
 
-echo 'through second case'
+
+
+echo ' ---> Target directory: ' $TARGET_DIR
 
 
 
-echo TARGET_DIRECTORY: $TARGET_DIR
 
-MUSHROOM_DIRECTORY=$PWD
-echo MUSHROOM_DIRECTORY: $MUSHROOM_DIRECTORY
+echo ''
+echo 'setting up clean mushroom folder...'
 
+mkdir $TARGET_DIR
 
+cp -R $SOURCE_DIR $TARGET_DIR
 
-#echo 'setting up clean mushroom folder...'
-
-#echo $1
-#mkdir $1
-#cd $1
-#
-#mkdir .mushroom
 #cp -R /Users/matthewbradbury/Dropbox/development/Dapps/mushroom/.mushroom/ ./.mushroom
 #cp /Users/matthewbradbury/Dropbox/development/Dapps/mushroom/.mushroom_config.js .
 #cp /Users/matthewbradbury/Dropbox/development/Dapps/mushroom/mushroom.js .
